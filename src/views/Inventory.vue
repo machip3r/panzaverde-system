82c6474aa5ac612a944e98a574f529a0ecf0d75d
DiegoAGtz <diegogtz1207@gmail.com>
2023-08-30 22:55:36 -0600

    First commit for inventory

--- src/views/Inventory.vue
+++ src/views/Inventory.vue
@@ -1,7 +1,62 @@
-<script setup></script>
+<script setup>
+ import { ref } from 'vue'
+ const inventory = ref([])
+ function getInventory() {
+     // Hacer petición axios para el inventario.
+     inventory.value.push(
+         {
+             producto: 'Frozen Yogurt',
+             precio: 200,
+             stock: '5 KG',
+             opciones: 'Nada papi',
+         }
+     )
+ }
+
+ getInventory()
+
+</script>
 
 <template>
-  <h1>Inventario</h1>
-</template>
+    <h2 class="mt-5">Inventario</h2>
 
-<script></script>
+    <v-text-field
+        class="mx-10 my-5"
+        density="compact"
+        variant="solo"
+        placeholder="Search templates"
+        prepend-inner-icon="mdi-filter"
+        bg-color="grey-lighten-3"
+        single-line
+        hide-details
+        flat
+        rounded
+    ></v-text-field>
+
+    <v-table
+        class="mx-5"
+        fixed-header
+        dense
+    >
+        <thead>
+            <tr>
+                <th class="text-center">Producto</th>
+                <th class="text-center">Precio</th>
+                <th class="text-center">Stock</th>
+                <th class="text-center">Opciones</th>
+            </tr>
+        </thead>
+        <tbody class="text-center">
+            <tr
+                v-for="item in inventory"
+                :key="item.producto"
+            >
+                <td>{{ item.producto }}</td>
+                <td>{{ item.precio }}</td>
+                <td>{{ item.stock }}</td>
+                <td>{{ item.opciones }}</td>
+            </tr>
+        </tbody>
+    </v-table>
+
+</template>
